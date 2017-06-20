0ecd40e3aadc67415ab14c25b63dd991f42e3ec9
huanhuan <huanfuan@163.com>
2017-06-21 00:38:23 +0800

    beetl-bbs 1.0.1搭建框架

---
+++ src/main/java/com/huan/SpringbootBeetl01Application.java
@@ -0,0 +1,13 @@
+package com.huan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeetl01Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootBeetl01Application.class, args);
+	}
+}
